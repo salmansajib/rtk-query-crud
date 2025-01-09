9576eb6555a6779d1d240f9d58c7ca7d49296fc3
Salman-sajib <sfsajib@gmail.com>
2025-01-09 14:36:10 +0600

    added all the functionality and modified some styles

--- src/features/todos/TodoList.jsx
+++ src/features/todos/TodoList.jsx
@@ -41,20 +41,19 @@ function TodoList() {
       {/* <label htmlFor="new-todo" className="">
         Enter a new todo
       </label> */}
-      <div className="flex items-center space-x-2 mt-1">
-        <div className="new-todo w-full">
-          <input
-            className="w-full border border-gray-300 rounded-md px-2 py-1 bg-neutral-50"
-            type="text"
-            value={newTodo}
-            id="new-todo"
-            onChange={(e) => setNewTodo(e.target.value)}
-            placeholder="Enter new todo"
-            autoComplete="off"
-          />
-        </div>
-        <button className="submit bg-gray-950 text-gray-50 py-1 px-2 rounded-md">
-          <IoMdAdd size={24} />
+      <div className="flex items-center space-x-2 mt-1 h-[45px]">
+        <input
+          className="w-full h-full text-lg border border-gray-300 rounded-md px-2 py-1 bg-gray-50"
+          type="text"
+          value={newTodo}
+          id="new-todo"
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+          autoComplete="off"
+        />
+
+        <button className="h-full bg-gray-950 text-gray-50 px-3 rounded-md">
+          <IoMdAdd size={36} />
         </button>
       </div>
     </form>
@@ -68,11 +67,13 @@ function TodoList() {
       return (
         <article
           key={todo.id}
-          className="bg-green-200 flex items-center justify-between px-2 py-1 rounded-md"
+          className={` flex items-center justify-between px-2 py-1 rounded-md ${
+            todo.completed ? "bg-red-100" : "bg-green-200"
+          }`}
         >
           <div>
             <input
-              className="mr-2"
+              className="mr-2 cursor-pointer"
               type="checkbox"
               checked={todo.completed}
               id={todo.id}
@@ -80,10 +81,20 @@ function TodoList() {
                 updateTodo({ ...todo, completed: !todo.completed })
               }
             />
-            <label htmlFor={todo.id}>{todo.title}</label>
+            <label
+              htmlFor={todo.id}
+              className={`font-medium text-lg ${
+                todo.completed ? "line-through text-gray-600" : ""
+              }`}
+            >
+              {todo.title}
+            </label>
           </div>
-          <button onClick={() => deleteTodo({ id: todo.id })}>
-            <MdDelete />
+          <button
+            className="text-gray-950 hover:text-red-500 transition-colors"
+            onClick={() => deleteTodo({ id: todo.id })}
+          >
+            <MdDelete size={24} />
           </button>
         </article>
       );
@@ -93,7 +104,7 @@ function TodoList() {
   }
 
   return (
-    <main className="bg-neutral-50 min-h-screen flex items-center justify-center px-2 py-10">
+    <main className="bg-gray-50 min-h-screen flex items-center justify-center px-2 py-10">
       <div className="w-full max-w-[500px]">
         <h1 className="text-4xl font-semibold mb-5 text-center">Todo List</h1>
         {newItemSection}
